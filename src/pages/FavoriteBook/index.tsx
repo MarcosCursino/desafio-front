e02349ad7311c71e88cd2f38a5f85c16e0e442f2
Marcos Pereira <marcos.pereira@rcky.com.br>
2022-02-19 13:23:02 -0300

    adjusted layout favorites

--- src/pages/FavoriteBook/index.tsx
+++ src/pages/FavoriteBook/index.tsx
@@ -3,15 +3,17 @@ import { Card } from "../../components/Card/Card";
 import { FavoriteContext } from "../../contexts/FavoriteContext";
 import styles from "./FavoriteBook.module.scss";
 export function FavoriteBook() {
-  const { books,handleDeleteBook } = useContext(FavoriteContext);
+  const { books, handleDeleteBook } = useContext(FavoriteContext);
 
   return (
     <div className={styles.container}>
-      <h1>Lista de Favoritos</h1>
+      <h2>Lista de Favoritos</h2>
 
-      {books?.map((item: any, index: any) => (
-        <Card item={item} key={index} index={index} pageFavorite />
-      ))}
+      <div className={styles.containerItens}>
+        {books?.map((item: any, index: any) => (
+          <Card item={item} key={index} index={index} pageFavorite />
+        ))}
+      </div>
     </div>
   );
 }
